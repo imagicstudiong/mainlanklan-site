d4e752f07106c59117a4fa6fec3db554e7487995
temitopeohassan <temitope.hassan.2@gmail.com>
2022-10-23 17:14:56 +0100

    correction

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import IMG from "../../assets/img/logo/logo.png"
 import Logo from '../../assets/img/logo.png';
 import { Link } from 'react-router-dom';
 
